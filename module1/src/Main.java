0d0db5f7635522dd67b0d4899aa4a6bb904441d6
renpengfei <13571804740@163.com>
2023-03-07 21:57:54 +0800

    我提交代码了

---
+++ module1/src/Main.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println("haha");
+
+    }
+
+    public static void main1(String[] args) {
+        ArrayList<String> list1 = new ArrayList<>();
+
+    }
+
+}
